c8c9b403612f1a93e1f2258e881f808a1ecc8f46
Emad Shaaban <emad.shaaban92@gmail.com>
2017-08-09 00:29:46 +0200

    syncMiddleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
 import eventChangeHandlerMiddleware from './event_change_handler_middleware';
 import clickNotificationMiddleware from './click_notification_middleware';
+import syncMiddleware from './sync_middleware';
 
 export default [
   eventChangeHandlerMiddleware,
-  clickNotificationMiddleware
+  clickNotificationMiddleware,
+  syncMiddleware
 ]

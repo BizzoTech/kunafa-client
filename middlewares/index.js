1ee8286eb70d27939bd9e90d8b68ffdddea6e994
Emad Shaaban <emad.shaaban92@gmail.com>
2017-08-09 02:41:32 +0200

    event sourcing middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 import eventChangeHandlerMiddleware from './event_change_handler_middleware';
 import clickNotificationMiddleware from './click_notification_middleware';
 import syncMiddleware from './sync_middleware';
+import eventSourcingMiddleware from './event_sourcing_middleware';
 
 export default [
   eventChangeHandlerMiddleware,
   clickNotificationMiddleware,
-  syncMiddleware
+  syncMiddleware,
+  eventSourcingMiddleware
 ]

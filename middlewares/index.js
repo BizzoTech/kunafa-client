24ee3d30d5f8cd253992b90490e0a973f507ac60
Emad Shaaban <emad.shaaban92@gmail.com>
2017-08-09 03:21:55 +0200

    local cache middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
+import localCacheMiddleware from './local_cache_middleware';
 import eventChangeHandlerMiddleware from './event_change_handler_middleware';
 import clickNotificationMiddleware from './click_notification_middleware';
 import syncMiddleware from './sync_middleware';
 import eventSourcingMiddleware from './event_sourcing_middleware';
 
 export default [
+  localCacheMiddleware,
   eventSourcingMiddleware,
   syncMiddleware,
   clickNotificationMiddleware,

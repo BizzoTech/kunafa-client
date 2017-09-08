ab484f9a899f8ff45fad80408512cbc0e81e529a
Emad Shaaban <emad.shaaban92@gmail.com>
2017-09-08 16:45:42 +0200

    test LOAD_EVENTS action

--- src/reducers/__tests__/events.test.js
+++ src/reducers/__tests__/events.test.js
@@ -9,5 +9,41 @@ describe("Events reducer", () => {
     expect(events(undefined, {})).toEqual(defaultState);
   });
 
+  it('should handle LOAD_EVENTS', () => {
+    const state = {}
+    const action = {
+      type: 'LOAD_EVENTS',
+      events: [
+        {
+          _id: 'event_2',
+          _rev: '1',
+          relevantDocsIds: ['doc_1'],
+          createdAt: 10
+        },
+        {
+          _id: 'event_1',
+          _rev: '1',
+          relevantDocsIds: ['doc_1'],
+          createdAt: 20
+        }
+      ]
+    }
+    const result = {
+      event_1: {
+        _id: 'event_1',
+        _rev: '1',
+        relevantDocsIds: ['doc_1'],
+        createdAt: 20
+      },
+      event_2: {
+        _id: 'event_2',
+        _rev: '1',
+        relevantDocsIds: ['doc_1'],
+        createdAt: 10
+      }
+    }
+    Reducer(events).withState(state).expect(action).toReturnState(result);
+  });
+
 
 });

48c4e7aeb618681a9c6a5994e15416a9dd434e75
Emad Shaaban <emad.shaaban92@gmail.com>
2017-09-08 19:13:52 +0200

    processing local reducer tests

--- src/reducers/__tests__/processing_local.test.js
+++ src/reducers/__tests__/processing_local.test.js
@@ -11,5 +11,45 @@ describe("Processing local reducer", () => {
     expect(processing_local(undefined, {})).toEqual(defaultState);
   });
 
+  it('should handle START_PROCESSING_LOCAL', () => {
+    const state = defaultState;
+    const action = {
+      type: 'START_PROCESSING_LOCAL',
+      progress: 75
+    }
+    const result = {
+      isProcessing: true,
+      progress: 75
+    }
+    Reducer(processing_local).withState(state).expect(action).toReturnState(result);
+  });
+
+  it('START_PROCESSING_LOCAL should be able to update progress', () => {
+    const state = {
+      isProcessing: true,
+      progress: 75
+    }
+    const action = {
+      type: 'START_PROCESSING_LOCAL',
+      progress: 85
+    }
+    const result = {
+      isProcessing: true,
+      progress: 85
+    }
+    Reducer(processing_local).withState(state).expect(action).toReturnState(result);
+  });
+
+  it('should handle END_PROCESSING_LOCAL', () => {
+    const state = {
+      isProcessing: true,
+      progress: 75
+    }
+    const action = {
+      type: 'END_PROCESSING_LOCAL'
+    }
+    const result = defaultState
+    Reducer(processing_local).withState(state).expect(action).toReturnState(result);
+  });
 
 });

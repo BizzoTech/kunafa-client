4c9451ce172002c1853e1ac89acd6a0cda441748
Emad Shaaban <emad.shaaban92@gmail.com>
2017-09-10 16:41:29 +0200

    notifications action creator test

---
+++ src/actionCreators/__tests__/notifications.test.js
@@ -0,0 +1,25 @@
+import {clickNotification, clickExternalNotification} from '../notifications';
+
+describe('Notifications action creators test', () => {
+
+  it('clickNotification should return CLICK_NOTIFICATION action', () => {
+    const notification = {
+      name: 'TEST_NOTIFICATION'
+    }
+    expect(clickNotification(notification)).toEqual({
+      type: 'CLICK_NOTIFICATION',
+      notification
+    })
+  });
+
+  it('clickExternalNotification should return CLICK_NOTIFICATION action marked as external', () => {
+    const notification = {
+      name: 'TEST_NOTIFICATION'
+    }
+    expect(clickExternalNotification(notification)).toEqual({
+      type: 'CLICK_NOTIFICATION',
+      notification,
+      external: true
+    })
+  });
+});

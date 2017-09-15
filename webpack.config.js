2d706d5ccdf093c5300c4aa02cec9a86a173cb7b
Emad Shaaban <emad.shaaban92@gmail.com>
2017-09-15 17:38:38 +0200

    umd support

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require('webpack');
+
+
+const path = require('path');
+
+if (!debug) {
+  plugins.push(new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production')
+    }
+  }));
+}
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/",
+    library: 'kunafa',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    filename: debug
+      ? 'js/kunafa.bundle.js'
+      : 'js/kunafa.bundle.min.js'
+  },
+  externals: {
+    react: "react"
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}

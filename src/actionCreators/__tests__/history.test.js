f3439907f00b5e73a492e943157646412d227b33
Emad Shaaban <emad.shaaban92@gmail.com>
2018-01-21 16:51:44 +0200

    history action creators tests

--- src/actionCreators/__tests__/history.test.js
+++ src/actionCreators/__tests__/history.test.js
@@ -7,51 +7,39 @@ describe("History action creators test", () => {
     });
   });
 
-  // it('navigateTo should return NAVIGATE_TO action with the passed routeName and params', () => {
-  //   const routeName = 'TEST_ROUTE';
-  //   const params = {
-  //     p1: 'v1'
-  //   }
-  //   expect(navigateTo(routeName, params)).toEqual({
-  //     type: 'NAVIGATE_TO',
-  //     route: {
-  //       name: routeName,
-  //       params
-  //     }
-  //   });
-  // });
-  //
-  // it('transiteTo should return TRANSITE_TO action with the passed routeName and params', () => {
-  //   const routeName = 'TEST_ROUTE';
-  //   const params = {
-  //     p1: 'v1'
-  //   }
-  //   expect(transiteTo(routeName, params)).toEqual({
-  //     type: 'TRANSITE_TO',
-  //     route: {
-  //       name: routeName,
-  //       params
-  //     }
-  //   });
-  // });
-  //
-  // it('goTo should return GO_TO action with the passed routeName and params', () => {
-  //   const routeName = 'TEST_ROUTE';
-  //   const params = {
-  //     p1: 'v1'
-  //   }
-  //   expect(goTo(routeName, params)).toEqual({
-  //     type: 'GO_TO',
-  //     route: {
-  //       name: routeName,
-  //       params
-  //     }
-  //   });
-  // });
-  //
-  // it('goBack should return GO_BACK action', () => {
-  //   expect(goBack()).toEqual({
-  //     type: 'GO_BACK'
-  //   });
-  // });
+  it("navigateTo should return NAVIGATE_TO action with the passed routeName and params", () => {
+    const path = ["TEST_ROUTE"];
+    expect(navigateTo(path)).toEqual({
+      type: "NAVIGATE_TO",
+      route: {
+        path
+      }
+    });
+  });
+
+  it("transiteTo should return TRANSITE_TO action with the passed routeName and params", () => {
+    const path = ["TEST_ROUTE"];
+    expect(transiteTo(path)).toEqual({
+      type: "TRANSITE_TO",
+      route: {
+        path
+      }
+    });
+  });
+
+  it("goTo should return GO_TO action with the passed routeName and params", () => {
+    const path = ["TEST_ROUTE"];
+    expect(goTo(path)).toEqual({
+      type: "GO_TO",
+      route: {
+        path
+      }
+    });
+  });
+
+  it("goBack should return GO_BACK action", () => {
+    expect(goBack()).toEqual({
+      type: "GO_BACK"
+    });
+  });
 });

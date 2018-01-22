7c7b5e5695fc97dbb66974be32988359c2738d3a
Emad Shaaban <emad.shaaban92@gmail.com>
2018-01-23 00:36:19 +0200

    getActionsFromPaths tests

--- src/middlewares/sync_middleware/__tests__/getActionsFromPaths.test.js
+++ src/middlewares/sync_middleware/__tests__/getActionsFromPaths.test.js
@@ -10,4 +10,191 @@ describe("getActionsFromPaths test", () => {
     };
     expect(getDocs({}, action)).toEqual([action.doc]);
   });
+
+  it("should transform syncPaths to actions grouped by insert, update and remove", () => {
+    const syncPaths = [
+      {
+        name: "type1",
+        filter: doc => doc.type === "type1",
+        actions: {
+          insert: "INSERT_TYPE1",
+          update: "UPDATE_TYPE1",
+          remove: "REMOVE_TYPE1",
+          load: "LOAD_TYPE1"
+        }
+      },
+      {
+        name: "type2",
+        filter: doc => doc.type === "type2",
+        actions: {
+          insert: "INSERT_TYPE2",
+          update: "UPDATE_TYPE2",
+          remove: "REMOVE_TYPE2"
+        }
+      }
+    ];
+
+    const mergedActions = getActionsFromPaths(syncPaths);
+    expect(mergedActions).toEqual({
+      insert: [
+        {
+          type: "INSERT_TYPE1",
+          getDocs
+        },
+        {
+          type: "INSERT_TYPE2",
+          getDocs
+        }
+      ],
+      update: [
+        {
+          type: "UPDATE_TYPE1",
+          getDocs
+        },
+        {
+          type: "UPDATE_TYPE2",
+          getDocs
+        }
+      ],
+      remove: [
+        {
+          type: "REMOVE_TYPE1",
+          getDocs
+        },
+        {
+          type: "REMOVE_TYPE2",
+          getDocs
+        }
+      ]
+    });
+  });
+
+  it("actions can be objects", () => {
+    const syncPaths = [
+      {
+        name: "type1",
+        filter: doc => doc.type === "type1",
+        actions: {
+          insert: {
+            type: "INSERT_TYPE1"
+          },
+          update: "UPDATE_TYPE1",
+          remove: "REMOVE_TYPE1",
+          load: "LOAD_TYPE1"
+        }
+      },
+      {
+        name: "type2",
+        filter: doc => doc.type === "type2",
+        actions: {
+          insert: "INSERT_TYPE2",
+          update: "UPDATE_TYPE2",
+          remove: "REMOVE_TYPE2"
+        }
+      }
+    ];
+
+    const mergedActions = getActionsFromPaths(syncPaths);
+    expect(mergedActions).toEqual({
+      insert: [
+        {
+          type: "INSERT_TYPE1",
+          getDocs
+        },
+        {
+          type: "INSERT_TYPE2",
+          getDocs
+        }
+      ],
+      update: [
+        {
+          type: "UPDATE_TYPE1",
+          getDocs
+        },
+        {
+          type: "UPDATE_TYPE2",
+          getDocs
+        }
+      ],
+      remove: [
+        {
+          type: "REMOVE_TYPE1",
+          getDocs
+        },
+        {
+          type: "REMOVE_TYPE2",
+          getDocs
+        }
+      ]
+    });
+  });
+
+  it("actions can be arrays of strings or objects", () => {
+    const syncPaths = [
+      {
+        name: "type1",
+        filter: doc => doc.type === "type1",
+        actions: {
+          insert: {
+            type: "INSERT_TYPE1"
+          },
+          update: [
+            "UPDATE_TYPE1",
+            {
+              type: "UPDATE_TYPE1_2"
+            }
+          ],
+          remove: "REMOVE_TYPE1",
+          load: "LOAD_TYPE1"
+        }
+      },
+      {
+        name: "type2",
+        filter: doc => doc.type === "type2",
+        actions: {
+          insert: ["INSERT_TYPE2"],
+          update: "UPDATE_TYPE2",
+          remove: ["REMOVE_TYPE2"]
+        }
+      }
+    ];
+
+    const mergedActions = getActionsFromPaths(syncPaths);
+    expect(mergedActions).toEqual({
+      insert: [
+        {
+          type: "INSERT_TYPE1",
+          getDocs
+        },
+        {
+          type: "INSERT_TYPE2",
+          getDocs
+        }
+      ],
+      update: [
+        {
+          type: "UPDATE_TYPE1",
+          getDocs
+        },
+        {
+          type: "UPDATE_TYPE1_2",
+          getDocs
+        },
+        {
+          type: "UPDATE_TYPE2",
+          getDocs
+        }
+      ],
+      remove: [
+        {
+          type: "REMOVE_TYPE1",
+          getDocs
+        },
+        {
+          type: "REMOVE_TYPE2",
+          getDocs
+        }
+      ]
+    });
+  });
 });
